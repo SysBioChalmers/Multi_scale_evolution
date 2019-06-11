64c8978ad3ae73caaf5de56c5a52682e0cea665a
hongzhonglu <luho@chalmers.se>
2019-06-11 21:27:06 +0200

    fix: refine the code to parse the active site of proteins.

--- mutation mapping analysis/2.2 parse_active_site.R
+++ mutation mapping analysis/2.2 parse_active_site.R
@@ -1,2 +1,14 @@
 # load packages
 activeSite <- mergeActiveSiteInf()
+
+
+# an example
+# P17157
+
+site_P17157 <- filter(activeSite, Entry =='P17157')
+
+# by comparison, it still lack:
+# 1. secondary strucuture
+# 2. Mutagenesis (Pathology & Biotech)
+# 3. Domain
+# 4. Sites
