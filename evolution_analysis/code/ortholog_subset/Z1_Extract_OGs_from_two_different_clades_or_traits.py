237523f8db51cdf8566aa489e53a70360f0cb272
hongzhonglu <luho@chalmers.se>
2020-09-06 17:37:10 +0200

    feat: add the script to do the repeat calculation using the crabtree effect

--- evolution_analysis/code/ortholog_subset/Z1_Extract_OGs_from_two_different_clades_or_traits.py
+++ evolution_analysis/code/ortholog_subset/Z1_Extract_OGs_from_two_different_clades_or_traits.py
@@ -258,3 +258,54 @@ for og in og_initial:
         species_background1 = len(set(pro_id) & set(species_background))
     if species_foreground1 >=3 and species_background1 >=3:
         shutil.copy(input_dir, cds_out)
+
+
+
+
+###############################################################################
+# for the repeat calculation
+##################################################################################
+
+
+
+# crabtree condition
+# filter cds based on species number in the foreground and reference group
+##################################################################################
+cdssource1 = "/home/luhongzhong/ortholog_343_crabtree_2/cds_refine/"
+cds_out = "/home/luhongzhong/ortholog_343_crabtree_2/cds_refine_filter/"
+os.system("mkdir /home/luhongzhong/ortholog_343_crabtree_2/cds_refine_filter/")
+
+trait_inf = pd.read_excel("~/Documents/evolution_analysis/data/data_tree/genome_summary_332_yeasts_heat_Ethanol_updated_02_20.xlsx", sheet_name = "crabtree_effect")
+trait_inf = trait_inf[(trait_inf["crabtree_effect"] =="Yes") | (trait_inf["crabtree_effect"] =="No")]
+
+trait_inf0 = trait_inf[["old_species_id","crabtree_effect", "Major clade"]]
+trait_inf0.columns = ["species","trait", "clade"]
+
+# firstly choose the species with the interest traits and its clade information
+trait_yes = trait_inf0[(trait_inf0["trait"]=="+") | (trait_inf0["trait"]=="Yes")]
+trait_none = trait_inf0[(trait_inf0["trait"] != "+") & (trait_inf0["trait"] != "Yes")]
+species_foreground = trait_yes["species"].tolist()
+species_background = trait_none["species"].tolist()
+
+og_initial = os.listdir(cdssource1)
+for og in og_initial:
+    print(og)
+    #og ="OG1006_code.fasta" # test
+    input_dir = cdssource1 + og
+    OG_trim = list(SeqIO.parse(input_dir, "fasta"))
+    pro_id = []
+    species_foreground1 = 0
+    species_background1 = 0
+    for record in OG_trim:
+        print(record.id, len(list(record.seq)))
+        s0 = record.id
+        s1 = s0.split("@")[0]
+        pro_id.append(s1)
+        # calucate the intersection of species with the trait_yes and trait_none
+        species_foreground1 = len(set(pro_id) & set(species_foreground))
+        species_background1 = len(set(pro_id) & set(species_background))
+    if species_foreground1 >=3 and species_background1 >=3:
+        shutil.copy(input_dir, cds_out)
+
+
+
