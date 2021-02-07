fc5e72b910abe51be784302f4ef51d3a4c36d6c0
hongzhonglu <luho@chalmers.se>
2021-02-07 11:04:09 +0100

    fix: remove the old script to do cluster analysis based on 3D structures

--- mutation mapping analysis/1. load the depend packages.R
+++
@@ -1,9 +0,0 @@
-#install package
-#library(devtools)
-#install_github("hongzhonglu/Yeastspot3D",force = TRUE)
-
-library(Yeastspot3D)
-
-#load packages
-loadPackageForSNPanalysis()
-library(hongR) # some functions from hongR package were still used!
